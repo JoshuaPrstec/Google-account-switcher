3d5330bb030715a6026cd6c7336ae4915c61356f
JoshuaPrstec <140563313+JoshuaPrstec@users.noreply.github.com>
2024-03-18 16:01:58 +1100

    Update user-switch.js

--- user-switch.js
+++ user-switch.js
@@ -1,10 +1,10 @@
 chrome.browserAction.onClicked.addListener(function(tab) {
     var url = tab.url;
-    // Check if the URL is a Google service and contains '/u/0/' or '/u/1/'
-    if (url.includes('google.com') && (url.includes('/u/0/') || url.includes('/u/1/'))) {
-        // Toggle between '/u/0/' and '/u/1/'
-        var newUrl = url.replace(/\/u\/([01])\//, function(match, p1) {
-            return `/u/${p1 === '0' ? '1' : '0'}/`;
+    // Check if the URL is a Google service and contains '0/' or '1/'
+    if (url.includes('google.com') && (url.includes('/1/') || url.includes('/0/'))) {
+        // Toggle between '/1/' and '/0/'
+        var newUrl = url.replace(/\/([01])\//, function(match, p1) {
+            return `/${p1 === '0' ? '1' : '0'}/`;
         });
         chrome.tabs.update(tab.id, {url: newUrl});
     }
