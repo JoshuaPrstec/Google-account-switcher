6bc5b6bb3a3a99076879dede181ab667ffd94522
JoshuaPrstec <140563313+JoshuaPrstec@users.noreply.github.com>
2024-03-18 13:40:29 +1100

    Update user-switch.js

--- user-switch.js
+++ user-switch.js
@@ -2,8 +2,10 @@ chrome.browserAction.onClicked.addListener(function(tab) {
     var url = tab.url;
     // Check if the URL is a Google service and contains '/u/0/' or '/u/1/'
     if (url.includes('google.com') && (url.includes('/u/0/') || url.includes('/u/1/'))) {
-        // Switch between '/u/0/' and '/u/1/'
-        var newUrl = url.replace('/u/0/', '/u/1/').replace('/u/1/', '/u/0/');
+        // Toggle between '/u/0/' and '/u/1/'
+        var newUrl = url.replace(/\/u\/([01])\//, function(match, p1) {
+            return `/u/${p1 === '0' ? '1' : '0'}/`;
+        });
         chrome.tabs.update(tab.id, {url: newUrl});
     }
 });
