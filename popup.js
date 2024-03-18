6a40b6ecdfaabaee7d62321c195d5afa500527d0
JoshuaPrstec <140563313+JoshuaPrstec@users.noreply.github.com>
2024-03-18 13:20:40 +1100

    Delete popup.js

--- popup.js
+++
@@ -1,5 +0,0 @@
-chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
-  const currentUrl = tabs[0].url;
-  const newUrl = currentUrl.includes('/u/0/') ? currentUrl.replace('/u/0/', '/u/1/') : currentUrl.replace('/u/1/', '/u/0/');
-  chrome.tabs.update(tabs[0].id, { url: newUrl });
-});
