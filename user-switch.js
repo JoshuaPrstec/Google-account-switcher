843f63ae61d7b36ad275a0ed949ce0f54b493119
Joshua Prstec <140563313+JoshuaPrstec@users.noreply.github.com>
2024-03-22 07:42:13 +1100

    Update user-switch.js

--- user-switch.js
+++ user-switch.js
@@ -8,12 +8,12 @@ chrome.action.onClicked.addListener((tab) => {
     });
     chrome.tabs.update(tab.id, { url: newUrl });
   } else if (url.includes('google.com') && !url.includes('u/0') && !url.includes('u/1')) {
-    // Replace 'pli=1' or 'authuser=0' with 'authuser=1', and 'authuser=1' with 'authuser=0'
-    let newUrl = url.replace(/(pli=1)|(authuser=0)/g, 'authuser=1')
-                     .replace(/authuser=1/g, (match) => {
-                       // Ensure only the first instance is replaced when 'authuser=0' was not present
-                       return url.includes('authuser=0') ? 'authuser=0' : match;
-                     });
+    // Replace 'pli=1' or 'authuser=0' with 'authuser=1'
+    let newUrl = url.replace(/(pli=1)|(authuser=0)/g, 'authuser=1');
+    // Ensure only the first instance of 'authuser=1' is replaced when 'authuser=0' was not present
+    else if (!url.includes('authuser=0')) {
+      newUrl = newUrl.replace('authuser=1', 'authuser=0');
+    }
     chrome.tabs.update(tab.id, { url: newUrl });
   }
 });
